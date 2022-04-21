83c8d970afe62634e891bd6c1a35feb53067ab75
MNlegion <rkreuser30@gmail.com>
2022-04-20 22:53:19 -0500

    no real changes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,51 @@
-var pokemonList = "https://pokeapi.co/api/v2/pokemon/?limit=151"
+// Form ID
+const pokedexEl = document.querySelector('#user-form');
+// Input ID
+const nameInputEl = document.querySelector('#poke-name');
+var pokemonList = "https://pokeapi.co/api/v2/pokemon/?limit=10"
 
-fetch(pokemonList).then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+// fetch(pokemonList)
+//   .then(function (response) {
+//     response.json().then(function (data) {
+//       console.log(data.results);
+//     });
+//   });
+
+  // console.log('here we go again');
+  // fetch(pokemonList)
+  // .then(response => {
+  //   console.log(response);
+  //   response.json().then(data => {
+  //     console.log(data.results);
+  //   })
+  //   .then(data => {
+  //     document.getElementById('pokemon')
+  //   })
+  // })
+
+  var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var pokemonSearch = nameInputEl.value.trim();
+
+    if (pokemonSearch) {
+      getPokemon(pokemonSearch);
+      nameInputEl.value = "";
+    } else {
+      alert("Oh my, this Pokemon doesn't seem to exist!")
+    }
+    console.log(event);
+  };
+
+  var getPokemon = function() {
+    fetch(pokemonList + name)
+    .then(response => {
+      console.log(response);
+      response.json().then(data => {
+        console.log(data);
+        displayPokemon(data, name);
+      });
+    })
+  };
+
+  pokedexEl.addEventListener('submit', formSubmitHandler);
+
